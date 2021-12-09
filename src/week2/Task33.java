36a17a563d2d897d59eb72e654dd5eb32b978d7b
Tatev <tattadevosian@gmail.com>
2021-12-09 18:25:46 +0400

    new changes

--- src/week2/Task33.java
+++ src/week2/Task33.java
@@ -21,27 +21,28 @@ public class Task33 {
             for (int row = 1; row <= size; row++) {
                 for (int col = 1; col <= size; col++) {
                     if (row >= col) {
-                        System.out.print("#");
+                        System.out.print("*");
                     }
                 }
                 System.out.print(" ");
                 for (int col = 1; col <= size; col++) {
                     if (row <= col) {
-                        System.out.print("#");
+                        System.out.print("*");
                     }
                 }
                 System.out.print(" ");
                 for (int col = 1; col <= size; col++) {
                     if ((row + col) <= (size + 1)) {
-                        System.out.print("#");
+                        System.out.print("*");
                     }
                 }
                 System.out.print(" ");
                 for (int col = 1; col <= size; col++) {
                     if ((row + col) >= (size + 1)) {
-                        System.out.print("#");
+                        System.out.print("*");
                     }
                 }
+
                 System.out.println();
             }
         }
